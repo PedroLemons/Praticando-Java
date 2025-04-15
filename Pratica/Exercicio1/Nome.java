5a33bcb66c7be816ff5268cf41aa5eda7945fa31
Pedro Lemos <p.a.lemos1@gmail.com>
2025-04-15 10:15:10 -0300

    Relembrando o basico

---
+++ Pratica/Exercicio1/Nome.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class Nome {
+
+	public static void main(String[] nome) {
+		
+        String primeiroNome = nome[0];
+        String sobrenome = nome[1];
+
+        System.out.println(sobrenome + " " + primeiroNome);
+
+	}
+
+}
