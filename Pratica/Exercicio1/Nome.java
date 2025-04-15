95be123efa41a60b9a0ddc12350f6d89354cac70
Pedro Lemos <p.a.lemos1@gmail.com>
2025-04-15 10:36:35 -0300

    Adicionado entrada de dados com 'scanner' e separacao de linhas para legibilidade

--- Pratica/Exercicio1/Nome.java
+++ Pratica/Exercicio1/Nome.java
@@ -1,14 +1,23 @@
 package Pratica.Exercicio1;
 
+import java.util.Scanner;
+
 public class Nome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] nome) {
-		
-        String primeiroNome = nome[0];
-        String sobrenome = nome[1];
+        System.out.println("");
+        System.out.println("-------- INICIANDO O PROGRAMA --------");
 
-        System.out.println(sobrenome + " " + primeiroNome);
+        System.out.print("Digite seu primeiro nome: ");
+        String primeiroNome = sc.nextLine();
 
-	}
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = sc.nextLine();
 
+        System.out.println( primeiroNome + " " + sobrenome );
+
+        sc.close();
+    }
 }
+
