5296c2664737dea219db0eb836583ff5d7c1eea8
Pedro Lemos <p.a.lemos1@gmail.com>
2025-04-27 19:31:28 -0300

    Geracao de fibonacci para pratica e revisao de estruturas basicas

---
+++ Pratica/Exercicio2/Fibonacci.java
@@ -0,0 +1,31 @@
+package Pratica.Exercicio2;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+   
+        System.out.println("");
+        System.out.println("-------- INICIANDO O PROGRAMA --------");
+
+        System.out.print("Digite um numero inteiro positivo: ");
+        int limite = scanner.nextInt();
+
+        int num1 = 0;
+        int num2 = 1;
+        int proxNum = 0;
+
+        System.out.print("Serie Fibonacci " + num1 + ", " + num2);
+
+        while (proxNum < limite) {
+            proxNum = num1 + num2;
+
+            System.out.print(", " + proxNum);
+            num1 = num2;
+            num2 = proxNum;
+        }
+    }
+
+}
